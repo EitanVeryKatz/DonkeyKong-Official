bd9c8dcc5a45ed951c9333f6d2e24a57ee712c04
EitanVeryKatz <eitanka@mta.ac.il>
2024-11-22 18:37:27 +0200

    added class for storing display info and floor arr

--- DonkeyKong Official/gameConfig.cpp
+++ DonkeyKong Official/gameConfig.cpp
@@ -1,5 +1,8 @@
 #include "gameConfig.h"
 #include <iostream>
+#include <algorithm>
+
+class Floor;
 
 void gotoxy(int x, int y)
 {
@@ -17,4 +20,15 @@ void showCurserOnConsole(bool showFlag)
 	GetConsoleCursorInfo(out, &cursorInfo);
 	cursorInfo.bVisible = showFlag; // set the cursor visibility
 	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void DisplayLayout::addPlatform(Floor* newPlatform) {
+	Floor** newArr = new Floor * [PlatformNum + 1];
+	if (PlatformNum > 0) {//if currently game cpntains platforms
+		std::copy(platformArr, platformArr + PlatformNum, newArr);//copy existing platforms to new arr
+		delete[]platformArr;//deleto old arr
+	}
+	newArr[PlatformNum] = newPlatform;//ad new platform to arr
+	platformArr = newArr;//update arr to current
+	PlatformNum++;//update number of platforms
 }
--- DonkeyKong Official/gameConfig.h
+++ DonkeyKong Official/gameConfig.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <windows.h>
+class Floor;
 
 void gotoxy(int x, int y);
 void showCurserOnConsole(bool showFlag);
 
+class DisplayLayout {//class to contain platforms, borders, and every non moving object displayed
+
+private:
+	int maxBoderX, minBorderX, maxBorderY, minBorderY;//border settings
+	int PlatformNum = 0;
+	Floor** platformArr = nullptr;
+public:
+	void addPlatform(Floor* newPlatform);//adds platform to platformArr and updates PlatformNum
+
+};
