cddc76a2639ea1c8b559fc184bde59fcf90cde53
jshef747 <j.sheffer747@gmail.com>
2024-11-26 13:52:44 +0200

    chnages to board

--- DonkeyKong Official/boardGame.h
+++ DonkeyKong Official/boardGame.h
@@ -13,15 +13,15 @@ class boardGame
 
 
 	const char* boardLayout[BOARD_HEIGHT] =
-	{
+	{   //           1         2         3         4         5         6         7                  
 		// 01234567890123456789012345678901234567890123456789012345678901234567890123456789
 		  "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ", // 0
-		  "Q                &&                                                            Q", // 1
-		  "Q                &&    $                                                       Q", // 2
-		  "Q             ==========                                                       Q", // 3
-		  "Q                                                                              Q", // 4
-		  "Q                                                                              Q", // 5
-		  "Q      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>H                                       Q", // 6
+		  "Q                                       &                                      Q", // 1
+		  "Q                                       &    $                                 Q", // 2
+		  "Q                                    =======H====                              Q", // 3
+		  "Q                                           H                                  Q", // 4
+		  "Q                                           H                                  Q", // 5
+		  "Q                                >>>>>>H>>>>>>>>>>>>>>>                        Q", // 6
 		  "Q                                      H                                       Q", // 7
 		  "Q                                      H                                       Q", // 8
 		  "Q                                    <<<<<<H<<<<<<<<<<<H<<<                    Q", // 9
