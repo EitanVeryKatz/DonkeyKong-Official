9f5c78bb2e51232c8404b8c1da3ea161a2fd1b7a
Jonathan Sheffer <j.sheffer747@gmail.com>
2024-12-11 08:55:44 +0200

    firstfloor updated

--- DonkeyKong Official/gameConfig.h
+++ DonkeyKong Official/gameConfig.h
@@ -4,7 +4,7 @@
 constexpr int LOW_DIFFCULTY = 0, MEDIUM_DIFFCULTY = 1, HIGH_DIFFCULTY = 2;
 constexpr int BOARD_WIDTH = 80;
 constexpr int BOARD_HEIGHT = 25;
-constexpr int firstFloorY = 18;
+constexpr int firstFloorY = 23;
 constexpr int BARRELS_NUM = 15;
 constexpr int diffculty[3] = { 3, 7, 10 };
 constexpr int BARREL_SPAWN_RATE = 20;
