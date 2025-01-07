b4133a1167e0a0963ba39b8df37583ffef0a3bc9
Jonathan Sheffer <j.sheffer747@gmail.com>
2025-01-07 17:38:51 +0200

    added bad_guys

--- DonkeyKong Official/ghost.h
+++ DonkeyKong Official/ghost.h
@@ -6,7 +6,6 @@ class ghost : public gameObject
 	static constexpr Direction directionsGhost[] = { {-1, 0}, {1, 0} };
 	static constexpr int dir_LEFT = 0, dir_RIGHT = 1, LEFT = -1, RIGHT = 1;
 	static constexpr char icon = 'x';
-	//point ghostPosition;
 	bool active = true;
 	bool smashed = false;
 	bool needChanged;
